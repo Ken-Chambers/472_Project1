d5623d622817c356a361f5cf267453f4dc8bd74a
Ken-Chambers <krc5625@psu.edu>
2023-10-30 13:37:06 -0400

    finished putting code in

--- build/main.py
+++ build/main.py
@@ -1,2 +1,26 @@
 from cli import createParser, main as cli_main
+from task_manager import TaskManager
+from thread_manager import ThreadManager
+
+def main():
+    parser = createParser()
+    args = parser.parse_args()
+
+    task_manager = TaskManager()
+    thread_manager = ThreadManager(task_manager)
+
+    if args.command == "add":
+        task_description = args.description
+        task_priority = args.priority
+        task_manager.addTask(task_description, task_priority)
+    elif args.command == "list":
+        task_manager.listTasks()
+    elif args.command == "run":
+        thread_manager.runTasks()
+    elif args.command == "exit":
+        thread_manager.stopThreads()
+    else:
+        print("Invalid command. Use 'add', 'list', 'run', or 'exit'.")
+
+main()
 
--- build/thread_manager.py
+++ build/thread_manager.py
@@ -1,4 +1,4 @@
-import threading, time
+import threading, time, queue
 
 class ThreadManager:
     def __init__(self, task_manager):
@@ -9,3 +9,20 @@ class ThreadManager:
     def executeTask(self):
         while not self.stop_threads_flag.is_set():
             try:
+                description = self.task_manager.tasks.get(block=False)
+                print(f"Executing task: {description}")
+
+                self.task_manager.tasks.task_done()
+            except queue.Empty:
+                break
+    
+    def runTasks(self):
+        for i in range(2):
+            thread = threading.Thread(target=self.executeTask)
+            self.threads.append(thread)
+            thread.start()
+
+    def stopThreads(self):
+        self.stop_threads_flag.set()
+        for thread in self.threads:
+            thread.join()
