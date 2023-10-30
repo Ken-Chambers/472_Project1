7d3e19abba7b5cf29297e3195dcb13b33e1e2c63
Ken-Chambers <krc5625@psu.edu>
2023-10-30 16:59:19 -0400

    fixed taskManager in main

--- build/main.py
+++ build/main.py
@@ -2,26 +2,28 @@ from cli import createParser, main as cli_main
 from task_manager import TaskManager
 from thread_manager import ThreadManager
 
+task_manager = TaskManager()
+thread_manager = ThreadManager(task_manager)
+
 def main():
     parser = createParser()
     args = parser.parse_args()
-
-    task_manager = TaskManager()
-    thread_manager = ThreadManager(task_manager)
 
     if args.command == "add":
         task_description = args.description
         task_priority = args.priority
         task_manager.addTask(task_description, task_priority)
+        print(f"Task '{task_description}' with priority {task_priority} has been added.")
+
     elif args.command == "list":
         tasks = task_manager.listTasks()
         if tasks:
             print("Tasks: ")
-            while not task_manager.tasks.empty():
-                description, priority = task_manager.tasks.get()
+            for description, priority in tasks:
                 print(f"Description: {description}, Priority: {priority}")
         else:
             print("No tasks to print, try adding a task first")
+        print("All tasks listed")
 
     elif args.command == "run":
         thread_manager.runTasks()
