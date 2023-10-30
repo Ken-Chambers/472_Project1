b20172438a8f47935088890858d6a0eca2185182
Ken-Chambers <krc5625@psu.edu>
2023-10-30 13:55:53 -0400

    updating to make code run

--- build/cli.py
+++ build/cli.py
@@ -2,7 +2,11 @@ import argparse
 
 def createParser():
     parser = argparse.ArgumentParser(description="Task Manager with Priority Queue")
-    
+    parser.add_argument("add", help="Add a new task")
+    parser.add_argument("edit", help="Edit an existing task")
+    parser.add_argument("list", help="List current task")
+    parser.add_argument("run", help="Run a new task")
+
 
     return parser
 
