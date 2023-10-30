a0133f5b700c628f6eba72bef00ba89bf6589b56
Ken-Chambers <krc5625@psu.edu>
2023-10-30 16:38:21 -0400

    still doing stuff

--- build/task_manager.py
+++ build/task_manager.py
@@ -5,11 +5,11 @@ class TaskManager:
         self.tasks = queue.PriorityQueue()
     
     def addTask(self, desc, prior):
-        self.tasks.put((-prior, desc))
+        self.tasks.put((prior, desc))
 
     def listTasks(self):
         tasks = []
         while not self.tasks.empty():
             description, priortiy = self.tasks.get()
-            tasks.append(description, priortiy)
-        return self.tasks
+            tasks.append((description, priortiy))
+        return tasks
